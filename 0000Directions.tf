05f3ee0f33b6d28e7f282290ceaec7f8d04b6755
Mark Spieker <Mark@SpiekerStudios.com>
2023-11-22 14:17:06 -0800

    updated directions

---
+++ 0000Directions.tf
@@ -0,0 +1,10 @@
+/*
+Challenge Directions
+
+Fun Challenge - (feel free to skip #1-#3)     #1) Git Clone the git linked at bottom (skip the fork step, go right to cloning on your local drive)  #2) MSG me on FB requesting to be added as a contributor - include your git email  #3) once added create YOUR OWN BRANCH (using your FB name) and start pushing updates to YOUR BRANCH of this project...  ***THE CHALLENGE***... If you know even a little about coding, here is a fun challenge. Build your own 00-Auth  |  01-VPC  |  02-Subnets files FROM SCRATCH. No git, no peeking at previous projects. Build the subnets using a count loop. And put a public/private subnet in each of 3 AZ's, and using a count loop launch an EC2 in each of the AZ's... This URL is all I used in addition to Terraform.io to make it happen. https://stackoverflow.com/.../terraform-create-ec2...
+https://github.com/InfiniteCloudGroup/Challenge03.git
+
+
+
+
+*/
